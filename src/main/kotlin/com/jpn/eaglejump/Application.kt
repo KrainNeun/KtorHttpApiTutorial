d217e4597b5a4baa3469bcfed229926d51bc7c08
KrainNeun <krainneun@icloud.com>
2021-12-09 15:39:28 +0900

    Order implement

--- src/main/kotlin/com/jpn/eaglejump/Application.kt
+++ src/main/kotlin/com/jpn/eaglejump/Application.kt
@@ -2,6 +2,7 @@ package com.jpn.eaglejump
 
 import com.jpn.eaglejump.routes.setCustomerRoutes
 import com.jpn.eaglejump.routes.setDefaultRoutes
+import com.jpn.eaglejump.routes.setOrderRoutes
 import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.serialization.*
@@ -17,5 +18,6 @@ fun Application.module() {
         }
         setDefaultRoutes()
         setCustomerRoutes()
+        setOrderRoutes()
     }.start(wait = true)
 }
